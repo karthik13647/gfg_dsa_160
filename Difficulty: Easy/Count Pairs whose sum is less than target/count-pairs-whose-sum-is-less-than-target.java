a96a7bf34f170316352888eb8ae8f9d1f75ba7ed
Karthik Batta <abhiprince4147@gmail.com>
2025-01-05 21:56:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Pairs whose sum is less than target/count-pairs-whose-sum-is-less-than-target.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+class Solution {
+    int countPairs(int arr[], int target) {
+        // Your code here
+        Arrays.sort(arr);
+        int i=0;
+        int cnt=0;
+        int j=arr.length-1;
+        while(i<j){
+            while(i<j && arr[i]+arr[j]>=target){
+                j-=1;
+            }
+            cnt+=j-i;
+            i+=1;
+        }
+        return cnt;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine());
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
