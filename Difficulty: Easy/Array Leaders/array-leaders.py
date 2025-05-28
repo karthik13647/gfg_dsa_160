0f6545e4139be250f08920a619e5c47bc281bd2e
Karthik Batta <abhiprince4147@gmail.com>
2025-05-28 12:11:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Leaders/array-leaders.py
@@ -0,0 +1,13 @@
+class Solution:
+    def leaders(self, arr):
+        # code here
+        n=len(arr)
+        ele=arr[n-1]
+        a=[ele]
+        
+        for i in range(n-2,-1,-1):
+            if ele<=arr[i]:
+                a.append(arr[i])
+                ele=arr[i]
+        b=a.reverse()
+        return a
