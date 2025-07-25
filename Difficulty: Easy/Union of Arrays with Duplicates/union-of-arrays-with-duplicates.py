42bd2d11eb832a3347b6ccab607e7552c9292769
Karthik Batta <abhiprince4147@gmail.com>
2025-07-25 12:55:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
@@ -1,7 +1,5 @@
-#User function Template for python3
-
 class Solution:    
-    #Function to return the count of number of elements in union of two arrays.
     def findUnion(self, a, b):
         # code here
-        return len(set(a+b))
+        a=a+b
+        return sorted(set(a))
